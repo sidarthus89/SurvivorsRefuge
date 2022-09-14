3c6a814e88a2600e98d796fa5bce20ebdd73a221
CommanderBeelo <xxxbilliamxxx@gmail.com>
2022-09-14 12:51:23 -0400

    Update config.cpp
    updated .cpp

--- SR_LoadingScreen/CustomMusic/config.cpp
+++ SR_LoadingScreen/CustomMusic/config.cpp
@@ -13,25 +13,21 @@ class CfgPatches
 	};
 };
 
+class CfgSoundSets
+{
+	class Main_Music_Menu_SoundSet
+	{
+		soundShaders[] = {"Main_Music_Menu_SoundShader"};
+		volumeFactor = 1;
+		frequencyFactor = 1;
+		spatial = 0;
+	};
+};	
 class CfgSoundShaders
 {
-	class SurvivorsRefuge_Music_1_SoundShader
+	class Main_Music_Menu_SoundShader
 	{
 		samples[] = {{"SurvivorsRefuge\SR_LoadingScreen\CustomMusic\StrivingForChange.ogg",1}};
-		volume=0.70794576;
-	};
-};
-
-class CfgSoundSets
-{	
-	class SurvivorsRefuge_Music_1_SoundSet
-	{
-		soundShaders[]=
-		{
-			"SurvivorsRefuge_Music_1_SoundShader"
-		};
-		volumeFactor=1;
-		frequencyFactor=1;
-		spatial=0;
+		volume = 1.00000000;
 	};
 };
